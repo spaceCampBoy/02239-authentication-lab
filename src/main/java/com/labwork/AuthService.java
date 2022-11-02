b4a80e495f018746c86b8cddb413039b9a9a78a6
magnus <mlj@aety.io>
2022-11-02 16:49:10 +0100

    session timestamp

--- src/main/java/com/labwork/AuthService.java
+++ src/main/java/com/labwork/AuthService.java
@@ -2,43 +2,42 @@ package com.labwork;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Scanner;
 
-import com.lambdaworks.crypto.SCrypt;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.*;
+
+
 import com.lambdaworks.crypto.SCryptUtil;
 
 public class AuthService implements AuthServiceInterface {
 
-	private ArrayList<String> tokenList;
+	private HashMap<String, Timestamp> tokenList;
 	private static final SecureRandom secureRandom = new SecureRandom(); //threadsafe
 	private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder(); //threadsafe
-	
-	
+
+
 	public AuthService() throws RemoteException {
 		super();
-		tokenList = new ArrayList<String>();
+		tokenList = new HashMap<>();
 		UnicastRemoteObject.exportObject(this, 0);
 	}
-	 
-	
-	
-	
-	
+
+
+
+
+
 	@Override
 	public String login(String username, String password) throws RemoteException, GeneralSecurityException  {
-		
+
 		//get the password from file
 		String storedPass = returnUserStoredPass(username);
-		
+
 		if (storedPass == null) {
 			System.out.println("(server): user '"+username+"' is not found");
 			return null;
@@ -53,41 +52,49 @@ public class AuthService implements AuthServiceInterface {
 		}
 	}
 
-	
+
 	private String returnUserStoredPass(String username) {
-		String hashedUserPass = null; 
+		String hashedUserPass = null;
 		try {
-		      File myObj = new File("./src/main/java/com/labwork/userlist.txt");
-		      Scanner myReader = new Scanner(myObj);
-		      while (myReader.hasNextLine()) {
-		        String data = myReader.nextLine();
-		        String[] lineSplit = data.split(":");
-		        
-		        if (lineSplit[0].equals(username) ) {
-		        	hashedUserPass = lineSplit[1];
-		        	break;
-		        }
-		      }
-		      myReader.close();
-		    } catch (FileNotFoundException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		 
-		 return hashedUserPass;
+			File myObj = new File("./src/main/java/com/labwork/userlist.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				String[] lineSplit = data.split(":");
+
+				if (lineSplit[0].equals(username) ) {
+					hashedUserPass = lineSplit[1];
+					break;
+				}
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+		return hashedUserPass;
 	}
-	
+
 	public String createSessionToken() {
-	    byte[] randomBytes = new byte[24];
-	    secureRandom.nextBytes(randomBytes);
-		String sessToken = base64Encoder.encodeToString(randomBytes);
-		tokenList.add(sessToken);
+		byte[] randomBytes = new byte[24];
+		secureRandom.nextBytes(randomBytes);
+		String sessToken =  base64Encoder.encodeToString(randomBytes);
+		tokenList.put(sessToken, Timestamp.from(Instant.now()));
+
 		return sessToken;
 	}
-	
-	
+
+
 	public Boolean checkSessionToken(String token) {
-		return tokenList.contains(token);
+		long milliseconds1 = tokenList.get(token).getTime();
+		long milliseconds2 = Timestamp.from(Instant.now()).getTime();
+		long diff = milliseconds2 - milliseconds1;
+		boolean diffAccept = diff / (60 * 1000) < 1;
+		if (diffAccept) {
+			tokenList.put(token, Timestamp.from(Instant.now()));
+		}
+		return tokenList.containsKey(token) && diffAccept;
 	}
 }
 
